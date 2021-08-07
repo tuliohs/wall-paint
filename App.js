710753fc16fde112d43ffc719e1464ade32048b7
Tulio0622 <tuliohhenriquepaz@gmail.com>
2021-08-07 08:36:19 -0300

    iniciando com reanimated

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Cubo from './src/components/Cubo';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Cubo />
       <StatusBar style="auto" />
     </View>
   );
